a89e872a3dfe6d7372838b0467c7eccfc5a1e172
Riadh BEL HADJ SALAH <rsalah@stcs.com.sa>
2020-01-12 18:17:07 +0300

    Add car entity

--- car-service/src/main/java/com/stcs/carservice/CarServiceApplication.java
+++ car-service/src/main/java/com/stcs/carservice/CarServiceApplication.java
@@ -1,8 +1,19 @@
 package com.stcs.carservice;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@EnableEurekaClient
 @SpringBootApplication
 public class CarServiceApplication {
 
@@ -10,4 +21,15 @@ public class CarServiceApplication {
 		SpringApplication.run(CarServiceApplication.class, args);
 	}
 
+}
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class Car {
+    @Id
+    private UUID id;
+    private String name;
+    private LocalDate releaseDate;
 }
